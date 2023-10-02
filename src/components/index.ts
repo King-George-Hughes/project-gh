dabf04ed2a8e1c1554368886190af3941c8653a1
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-02 00:07:51 +0000

    changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ import Heading from "./Heading";
 import BottomNavBar from "./BottomNavBar";
 import Service from "./Service";
 import Choose from "./Choose";
-import Work from "./work";
+import Work from "./Work";
 import Footer from "./Footer";
 
 export { Footer, NavBar, Heading, BottomNavBar, Service, Choose, Work };
