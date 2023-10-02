e8ec151be6597ea095f41b05bf4388efcca32081
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-02 00:20:08 +0000

    changes

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,53 +4,53 @@ const service = [
     title: "Cloud Backup Services",
     description:
       "The cloud is a versatile platform capable of supporting numerous virtual services, such as operating systems...",
-    image: "../../get-started.png",
+    image: "/get-started.png",
   },
   {
     id: 2,
     title: "VoIP (Voice Over Internet Protocol)",
     description:
       "The cloud is a versatile platform capable of supporting numerous virtual services, such as operating systems...",
-    image: "../../stamp.png",
+    image: "/stamp.png",
   },
   {
     id: 3,
     title: "Data Backup and Restoration",
     description:
       "The cloud is a versatile platform capable of supporting numerous virtual services, such as operating systems...",
-    image: "../../whats-new.png",
+    image: "/whats-new.png",
   },
 ];
 
 const choose = [
   {
     id: 1,
-    image: "../../public/home.png",
+    image: "/home.png",
     label: "High Quality Hardware",
   },
   {
     id: 2,
-    image: "../../public/chat.png",
+    image: "/chat.png",
     label: "Dedicated 24/7 Support",
   },
   {
     id: 3,
-    image: "../../public/mail.png",
+    image: "/mail.png",
     label: "30 Day money-back Guarantee",
   },
   {
     id: 4,
-    image: "../../public/photo.png",
+    image: "/photo.png",
     label: "Agile and Fast Working Style",
   },
   {
     id: 5,
-    image: "../../public/transcript.png",
+    image: "/transcript.png",
     label: "Some Apps are Free",
   },
   {
     id: 6,
-    image: "../../public/voice.png",
+    image: "/voice.png",
     label: "High Level of Usability",
   },
 ];
@@ -58,37 +58,37 @@ const choose = [
 const work = [
   {
     id: 1,
-    image: "../../public/web1.png",
+    image: "/web1.png",
     label: "Nike Air Commerce",
     date: "22 Dec 2021",
   },
   {
     id: 2,
-    image: "../../public/web2.png",
+    image: "/web2.png",
     label: "Eligant Nail Retail",
     date: "22 Dec 2021",
   },
   {
     id: 3,
-    image: "../../public/web3.png",
+    image: "/web3.png",
     label: "Data Onboarding Platform",
     date: "22 Dec 2021",
   },
   {
     id: 4,
-    image: "../../public/web4.png",
+    image: "/web4.png",
     label: "Smart Bouquest Delivery",
     date: "22 Dec 2021",
   },
   {
     id: 5,
-    image: "../../public/web5.png",
+    image: "/web5.png",
     label: "Work faster with Aimm",
     date: "22 Dec 2021",
   },
   {
     id: 6,
-    image: "../../public/web6.png",
+    image: "/web6.png",
     label: "Creative for Everyone",
     date: "22 Dec 2021",
   },
