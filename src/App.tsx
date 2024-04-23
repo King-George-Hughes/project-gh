37164a4ad7bb95f0d4307db5f5d25a48fd74f7a2
King-George-Hughes <hughesgeorgeking@gmail.com>
2024-04-23 15:49:01 +0000

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import "preline/preline";
 import { BottomNavBar, Footer, NavBar } from "./components";
 import { ChooseUs, Home, RecentWorks, Reviews, Services } from "./pages";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+declare global {
+  interface HSStaticMethods {
+    autoInit(): void;
+  }
+  interface Window {
+    HSStaticMethods: HSStaticMethods;
+  }
+}
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.HSStaticMethods.autoInit();
+  }, [location.pathname]);
+
   return (
     <div className="bg-gray-50 dark:bg-gradient-to-tr from-[#020122] from-20% via-[#032442] to-[#1A012E] dark:border-gray-600 dark:text-gray-100">
       <NavBar />
