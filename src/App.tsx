9a7951af7d4998605af56a55bdeaa82ed150e205
King-George-Hughes <hughesgeorgeking@gmail.com>
2024-04-23 16:04:23 +0000

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import "preline/preline";
-import { IStaticMethods } from "preline/preline";
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+// import "preline/preline";
+// import { IStaticMethods } from "preline/preline";
 import { BottomNavBar, Footer, NavBar } from "./components";
 import { ChooseUs, Home, RecentWorks, Reviews, Services } from "./pages";
-declare global {
-  interface Window {
-    HSStaticMethods: IStaticMethods;
-  }
-}
+// declare global {
+//   interface Window {
+//     HSStaticMethods: IStaticMethods;
+//   }
+// }
 
 function App() {
-  const location = useLocation();
+  // const location = useLocation();
 
-  useEffect(() => {
-    window.HSStaticMethods.autoInit();
-  }, [location.pathname]);
+  // useEffect(() => {
+  //   window.HSStaticMethods.autoInit();
+  // }, [location.pathname]);
 
   return (
     <div className="bg-gray-50 dark:bg-gradient-to-tr from-[#020122] from-20% via-[#032442] to-[#1A012E] dark:border-gray-600 dark:text-gray-100">
